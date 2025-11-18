46251e641d56a1547645a1ab15dc2421c9daf41c
drannnf <drflores2020@plm.edu.ph>
2025-11-18 19:38:50 +0800

    Deployment preparation

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from 'dotenv';
 import cors from "cors";
+import path from "path";
 
 import jobsRoutes from "./routes/jobsRoutes.js";
 import connectDB from "./config/db.js";
@@ -10,13 +11,17 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 //Middleware to parse JSON bodies
 app.use(express.json()); 
 app.use(rateLimiter);
-app.use(cors({
-  origin: 'http://localhost:5173'
-}));
+
+if (process.env.NODE_ENV !== 'production') {
+  app.use(cors({
+    origin: 'http://localhost:5173'
+  }));
+}
 
 app.use ((req, res, next) => {
     console.log(`${req.method} request for '${req.url}' `);
@@ -24,6 +29,14 @@ app.use ((req, res, next) => {
 });
 
 app.use("/api/jobs", jobsRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../frontend', 'dist', 'index.html'));
+  });
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
