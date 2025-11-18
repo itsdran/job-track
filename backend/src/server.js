d8e925a8843fd1c5428050158544f7106d16a0b0
drannnf <drflores2020@plm.edu.ph>
2025-11-19 00:20:52 +0800

    Fixed error in deployment preparation

--- backend/src/server.js
+++ backend/src/server.js
@@ -34,9 +34,9 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../frontend', 'dist', 'index.html'));
+    res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'));
   });
-}
+} 
 
 connectDB().then(() => {
   app.listen(PORT, () => {
