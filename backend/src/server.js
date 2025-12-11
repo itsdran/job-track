3bb99022ad8b76c0806fca88c10469f14fca1994
drannnf <drflores2020@plm.edu.ph>
2025-12-11 15:06:38 +0800

    test4

--- backend/src/server.js
+++ backend/src/server.js
@@ -37,7 +37,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/jobs", jobsRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  const frontendPath = path.join(__dirname, "../frontend/dist");
+  const frontendPath = path.join(__dirname, "../../frontend/dist");
   app.use(express.static(frontendPath));
 
   app.get("*", (req, res) => {
